a3b54fe6ea0cb7333b661a77e7191b1722316311
WilliamColton <forfun.william.c@gmail.com>
2023-12-11 17:00:31 +0800

    update

--- lib/GameEngine.py
+++ lib/GameEngine.py
@@ -16,10 +16,6 @@ class GameEngine(screen.Screen):
     def update(self):
         pass
 
-
 class GameObject(GameEngine):
-    def __init__(self, screen_x, screen_y,x,y,type):
-        super().__init__(screen_x, screen_y)
-        self.x=x
-        self.y=y
-        self.type=type
+    def __init__(self, i2c, addr=60, external_vcc=False):
+        super().__init__(i2c, addr, external_vcc)
