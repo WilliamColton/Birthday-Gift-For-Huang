a684456b625e6e80d46339a4b56ec0fc616b437e
WilliamColton <forfun.william.c@gmail.com>
2023-12-11 16:16:47 +0800

    Add GameEngine

--- lib/screen.py
+++ lib/screen.py
@@ -1,8 +1,6 @@
 import ssd1306
+
 class Screen(ssd1306.SSD1306_I2C):
 
-    def __init__(self,screen_length,screen_width,i2c):
-        self.x=screen_length
-        self.y=screen_width
-        self.i2c=i2c
-        
+    def __init__(self, i2c, addr=60, external_vcc=False):
+        super().__init__(i2c, addr, external_vcc)
